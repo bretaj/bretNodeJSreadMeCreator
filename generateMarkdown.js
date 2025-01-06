7d1a0ed0e254a91219d08f1e88fbaabaf8c1abf5
bret jensen <jensenbret13@gmail.com>
2025-01-05 16:29:42 -0800

    .

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -51,7 +51,7 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
     return `
-    # ${data.title}
+    # ${data.projectTitle}
     
     ## Description
     ${data.description}
